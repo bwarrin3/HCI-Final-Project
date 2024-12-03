affc2413a6250b11aaf595dba2b14acc7ca6405c
Brent Kent <bwarrin3@kent.edu>
2024-12-02 19:54:59 -0500

    Fix no index.html GET

--- database.js
+++ database.js
@@ -26,6 +26,10 @@ app.get("", function (req, res) {
     res.sendFile(__dirname + "/" + "index.html");
 });
 
+app.get("/index.html", function (req, res) {
+    res.sendFile(__dirname + "/" + "index.html");
+});
+
 app.get("/login.html", function (req, res) {
     res.sendFile(__dirname + "/" + "login.html");
 });
